afb2ad0e4ba247e54fb46acdf822a35ed6f68227
Alex S <alex.spitz@miuros.com>
2021-04-02 11:07:24 +0200

    Refactor

--- server/src/services/peopleService.ts
+++ server/src/services/peopleService.ts
@@ -1,13 +1,14 @@
-import { PeopleDTO } from "../dtos/peopleDTO";
+import { PeopleSignupDTO } from "../dtos/peopleSignupDTO";
 import { people } from "../models/people";
 import {location} from "../models/location"
 
 export default class PeopleService {
-  async save(model: PeopleDTO) {
+  async save(model: PeopleSignupDTO) {
     let p = new people()
     p.name = model.name
     let table = await location.findByPk(model.tableId);
     p.tableRef = table;
     p.save()
+    return p.get()
   }
 }
