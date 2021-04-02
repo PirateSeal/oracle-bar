9d9ea2352d6f0478d1184c38fd956242a69b70c5
Alex S <alex.spitz@miuros.com>
2021-04-02 10:24:57 +0200

    init people an table with sequelize

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,22 @@
 import express from 'express';
 
+
+import { Sequelize } from 'sequelize-typescript'
+import { People } from './models/people';
+import { TableRef } from './models/table';
+
+const sequelize = new Sequelize({
+  database: 'some_db',
+  dialect: 'sqlite',
+  username: 'root',
+  password: '',
+  storage: ':memory:',
+  models: [__dirname + '/models'] // or [Player, Team],
+})
+
+sequelize.addModels([People, TableRef])
+
+
 const app = express();
 const port = 3001;
 app.get('/', (req, res) => {
