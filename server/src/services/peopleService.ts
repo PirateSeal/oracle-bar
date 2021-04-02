66bdaa036262325eee36f958b5caa391c8a34e38
PirateSeal <tcousin.pseal@gmail.com>
2021-04-02 11:31:52 +0200

    Fix DTO save

--- server/src/services/peopleService.ts
+++ server/src/services/peopleService.ts
@@ -1,14 +1,14 @@
-import { PeopleSignupDTO } from "../dtos/peopleSignupDTO";
-import { People } from "../models/People";
+import {PeopleSignupDTO} from "../dtos/peopleSignupDTO";
+import {People} from "../models/People";
 import {Location} from "../models/Location"
 
 export default class PeopleService {
-    let p = new People()
-  async save(model: PeopleSignupDTO) {
-    p.name = model.name
-    let table = await Location.findByPk(model.tableId);
-    p.tableRef = table;
-    p.save()
-    return p
-  }
+
+    async Save(model: PeopleSignupDTO) {
+        let p = new People()
+        p.name = model.name
+        p.tableRef = await Location.findByPk(model.tableId);
+        await p.save()
+        return p
+    }
 }
